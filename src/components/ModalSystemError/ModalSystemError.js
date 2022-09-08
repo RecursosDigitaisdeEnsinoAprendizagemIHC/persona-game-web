27182a6bf30d43bb19096084b082f52d41b261c1
jpcirqueira <jpcirqueira81@gmail.com>
2022-09-08 00:08:22 -0300

    adicionado ModalSystemError

---
+++ src/components/ModalSystemError/ModalSystemError.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+import Button from '../Button/Button';
+import { Container, Card, Title, SubTitle, Body, CardContent, AlertImg} from "./ModalSystemError.style";
+
+const ModalSystemError = ({ openModal, error, message }) => {
+    const [isOpen, setIsOpen] = useState(openModal)
+     return (
+        <>
+            {
+                isOpen && 
+                <Container>
+                <Card>
+                    <CardContent>
+                    <AlertImg src="/alert_message.svg" />
+                    <Body>
+                        <Title>Erro no sistema</Title>
+                        <SubTitle>{`Erro: ${error}`}</SubTitle>
+                        <SubTitle>{`Mensagem: ${message}`}</SubTitle>
+                    </Body>
+                    </CardContent>
+    
+                    <Button
+                        onClick={() => setIsOpen(false)}
+                        color="secondary"
+                    >
+                        Fechar
+                    </Button>
+                </Card>
+            </Container>
+            }
+        </>
+        
+  );
+};
+
+export default ModalSystemError;
---
+++ src/components/ModalSystemError/ModalSystemError.style.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  z-index: 1001;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  background-color: rgba(229, 229, 229, 0.70);
+`;
+
+export const Card = styled.div`
+  width: 500px;
+  height: 300px;
+  border-radius: 10px;
+  background-color: #FFFFFF;
+  z-index: 1002;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  box-shadow: 3px 3px 3px black
+`;
+
+export const CardContent = styled.div`
+  width: 100%;
+  display: flex;
+  padding-left: 40px;
+  margin-bottom: 24px;
+`;
+
+export const Body = styled.div`
+  width: 100%;
+  padding-left: 40px;
+`;
+
+export const Title = styled.h3`
+  color: #F44236;
+`;
+
+export const SubTitle = styled.h3`
+  color: #03318C;
+  margin-bottom: 8px;
+  margin-top: 8px;
+`;
+
+export const AlertImg = styled.img`
+  padding-bottom: 98px;
+`;
