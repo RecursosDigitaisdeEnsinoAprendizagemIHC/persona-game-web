cf76c09037d32e7c94da9577cd76c759511a3f5e
heronsousa <heron.sousa@sensedata.com.br>
2023-01-31 23:14:01 -0300

    refactor: Utiliza componente compartilhado para o cabeçalho da pagina de fases

--- src/pages/phases-menu.js
+++ src/pages/phases-menu.js
@@ -1,23 +1,16 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 // components
 import Container from "../components/ui/Container/Container";
-import {
-  HeaderTitle,
-  HeaderBtn,
-  HeaderContainer,
-} from "../components/ui/phases/Phases.style";
 import PhaseList from "../components/PhaseList/PhaseList";
 import ModalSystemError from "../components/ModalSystemError/ModalSystemError";
-import { Tooltip } from 'react-tippy';
+
 // constants
-import theme from "../constants/theme";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPhases } from "../store/actions/phase.action";
 import Loading from "../components/loading/loading";
+import PageHeader from "../components/ui/PageHeader/PageHeader";
 
 const PhasesMenu = () => {
 
@@ -42,25 +35,15 @@ const PhasesMenu = () => {
   return (
     <>
     <Container>
-      <HeaderContainer>
-
-          <HeaderBtn onClick={()=>goBack()}>
-          <Tooltip
-          title={'Voltar para página principal'}
-          position="bottom"
-          animation="fade"
-          theme="transparent"
-          distance={2}
-        >
-            <FontAwesomeIcon icon={faArrowLeft} size="3x" color={theme.primary} />
-            </Tooltip>
-          </HeaderBtn>
-
-        <HeaderTitle>FASES</HeaderTitle>
-      </HeaderContainer>
-     {phases.code === undefined ? <PhaseList phases={phases} />:<ModalSystemError openModal error={phases.code} message={phases.message} />} 
-
+      <PageHeader onBack={() => goBack()} title="Fases" />
+      {phases.code === undefined ? (
+          <PhaseList phases={phases} />
+        ):(
+          <ModalSystemError openModal error={phases.code} message={phases.message} />
+        )
+      }
     </Container>
+
     {isLoading &&  <Loading />}
     </>
   );
